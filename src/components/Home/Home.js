bc30dcc0dddbd3fb1023babd72070769a8038598
alyson-b69 <alyson.bernabeu@gmail.com>
2020-07-17 15:02:39 +0200

    add image to home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,9 +5,12 @@ function Home(){
     return(
         <div className="home">
             <h2>Welcome to React Start</h2>
-            <p>Bienvenue à toi jeune Reacteur.</p>
+            <p>Bienvenue à toi jeune apprenti Reacteur.</p>
             <p>Nous avons créé ce petit site pour t'apporter quelques petites informations bien utiles quand on démarre l'apprentissage de React JS.</p>
             <p>Installe-toi confortablement, et bonne lecture !</p>
+            <br/>
+            <img src="https://xbsoftware.com/wp-content/uploads/2019/03/react-js-pict.png" alt="React illustration" title="credit photo : xbsoftware.com "/>
+        
         </div>
     
     )
