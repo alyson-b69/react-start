00534f26447ee2e66c86a68b8140a3a3cb4090b6
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-07-19 11:03:02 +0200

    add an intro and a paragraph to redux page

--- src/components/Redux/Redux.js
+++ src/components/Redux/Redux.js
@@ -1,5 +1,6 @@
-import React from "react"
-import "./Redux.css"
+import React from "react";
+import "./Redux.css";
+import modelFlux from '../../modelFlux.png';
 
 function Redux(){
 
@@ -25,10 +26,18 @@ return(
                 </div>
             </div>
 
-            <p>Paragraphe d'introduction ...</p>
+            <p>
+                Maintenant que tu sais comment organiser ton projet tu pense être sortie d'affaire. Hélas les choses ne sont pas si simple comme tout les Devs React avant toi quand ton application va grossir et se complexifier tu rencontrera un problème. Un problème tout simple et qui pourtant a mener certain projets à leur perte, ce problème est la gestion d'un état partagé par plusieurs composants isolés les uns des autres.
+            </p>
 
-            <h3>Sous titre</h3>
-            <p>Blablabla</p>
+            <h3>La naissance de Redux</h3>
+            <p>Pour comprendre la naissance de Redux il faut remonter un peu en arrière quand tout le monde se basait encore sur le modèle MVC(model vue controller) ou ses dérivés... Quand facebook a sortit React beaucoup de devs ont rencontré le problème citer dans l'introduction alors la communauté a demandé a facebook s'il n'avait pas une solution a apporter au problème. Facebook a alors présenter Flux.
+            </p>
+            <img src={modelFlux} alt="shema du modele Flux" className="fluxImg"></img>
+            <p>  
+            Flux c'est quoi ? Flux n'est ni un framework, ni une librairie, c'est simplement un modèle d'architecture pour mieux organiser son code React. Seulement Flux est peut être juste un modèle mais il est assez compliqué a mettre en place vu que pour pour chaque projet il fallait créer son propre Flux(oui car Flux nécessite quelques outils a faire soi même pour fonctionner). A cette époque tout le monde créait sa propre librairie Flux et c'était le bordel, jusqu'au jour où Dan Abramov décida de changer les choses en créant Redux
+            </p>
+            
 
             <h3>Sous titre</h3>
             <p>Contenue</p>
